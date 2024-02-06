15372de796b860ee595d3411247cef40826bd81f
hieunmh <hieunm.pt@gmail.com>
2024-02-06 22:47:26 +0700

    delete

--- src/components/home/Estimate.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Estimate() {
-  return (
-    <div className='w-[1100px] h-[500px] rounded bg-gray-200'>
-      Estimate
-    </div>
-  )
-}
--- src/components/home/OrderTracking.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function OrderTracking() {
-  return (
-    <div className='w-[1100px] h-[500px] rounded bg-red-200'>
-      123
-    </div>
-  )
-}
